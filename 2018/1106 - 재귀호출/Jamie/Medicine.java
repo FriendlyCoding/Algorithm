bfc4bbf721f2ea25ec0dcb11ea8c9e5052f81b02
jamieJeon <woqja0@gmail.com>
2018-11-07 09:32:19 +0900

    #2 Update Jamie's source with kiyeon's thinking

--- 2018/1106 - 재귀호출/Jamie/Medicine.java
+++ 2018/1106 - 재귀호출/Jamie/Medicine.java
@@ -14,47 +14,26 @@ public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        String input = "";
-
-        System.out.println("Input your numbers : ");
         int num = 0;
+
         do {
-            try {
-                String line = scan.nextLine();
-
-                num = Integer.parseInt(line);
-
-                if (num == 0) {
-                    continue; // 입력 종료
-                } else if (num <= 30 && num >= 1) {
-                    int target = Integer.parseInt(line);
-                    long[][] table = new long[target + 1][target + 1];
-                    System.out.println(mediCase(target, 0, table)); // 입력받은 수에 대한 결과 처리
-                } else {
-                    throw new Exception("Wrong input"); // 입력 오류 - 제한사항 어김
-                }
-            } catch (Exception e) {
-                System.out.println("ERROR!!! Input numbers : ");
-                scan = new Scanner(System.in); // 에러 처리
-            }
+            num = scan.nextInt();
+            long[][] table = new long[num + 1][num + 1];
+            System.out.println(mediCase(num, 0, table)); // 입력받은 수에 대한 결과 처리
         } while (num != 0); // 마지막에 0을 입력받으면 입력 종료
-
     }
 
     public static long mediCase(int w, int h, long[][] table){
-        long result = 0;
-
         if(table[w][h] == 0){
-            if(w != 1 && h == 0){
-                result += mediCase(w-1, 1, table); // 반개 알약이 하나도 없는 경우
-            } else if(w != 0 && h != 0){
-                result += (mediCase(w-1, h+1, table) + mediCase(w, h-1, table)); //전체 알약과 반개알약이 한개이상있는경우
-            } else if(w == 0){
-                result += 1; // 전체 알약이 없는 경우
+            if(w == 0){
+                table[w][h] = 1; // 전체 알약이 없는 경우
             } else if(w == 1 && h == 0){
-                result += 1; // 알약 한개인경우
+                table[w][h] = 1; // 알약 한개인경우
+            } else if(w != 1 && h == 0){
+                table[w][h] = mediCase(w-1, 1, table); // 반개 알약이 하나도 없는 경우
+            } else {
+                table[w][h] = (mediCase(w-1, h+1, table) + mediCase(w, h-1, table)); //전체 알약과 반개알약이 한개이상있는경우
             }
-            table[w][h] = result;
         }
 
         return table[w][h];
