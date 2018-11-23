120e819cc7d0bb2503c7720d1974527bf21e7db0
jamieJeon <woqja0@gmail.com>
2018-11-23 14:37:06 +0900

    #4 Update Jamie's source

--- 2018/1127 - 1차원 배열 사용하기/Jamie/B2577_NumCount.java
+++ 2018/1127 - 1차원 배열 사용하기/Jamie/B2577_NumCount.java
@@ -2,40 +2,25 @@ import java.util.Scanner;
 
 public class B2577_NumCount {
     public static void main(String[] args) {
-        String[] ascending = {"1","2","3","4","5","6","7","8"};
-        String[] descending = {"8","7","6","5","4","3","2","1"};
-
         Scanner scan = new Scanner(System.in);
-        String line = scan.nextLine();
+        int A = scan.nextInt();
+        int B = scan.nextInt();
+        int C = scan.nextInt();
 
-        String[] input = line.split(" ");
+        int ABC = A*B*C;
 
-        int type = 0;
+        int[] result = new int[10];
 
-        for(int i=0; i<input.length; i++){
-            if(input[i].equals(ascending[i])){
-                if(type != 0 && type != 1){
-                    type = 3;
-                    break;
-                }
-                type = 1;
-            } else if(input[i].equals(descending[i])){
-                if(type != 0 && type != 2){
-                    type = 3;
-                    break;
-                }
-                type = 2;
-            } else {
-                type = 3;
-                break;
-            }
+        while(ABC > 0){
+            int num = ABC % 10;
+
+            result[num] ++;
+
+            ABC = ABC / 10;
         }
 
-        switch (type){
-            case 1: System.out.println("ascending"); break;
-            case 2: System.out.println("descending"); break;
-            default: System.out.println("mixed"); break;
+        for(int i=0; i<result.length; i++){
+            System.out.println(result[i]);
         }
-
     }
 }
