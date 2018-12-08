10458d9c434504aaf8bb8b856fb081c4cd652d07
seongsilheo <huhss9981@gmail.com>
2018-12-08 23:33:50 +0900

    Update search_engine.cpp

--- 2018/1106_재귀호출/Seongsil/search_engine.cpp
+++ 2018/1106_재귀호출/Seongsil/search_engine.cpp
@@ -16,11 +16,7 @@ int main(void){
 
 	int cases;
 	string afta;
-	map<string, int> m = { { "A", 0 }, { "B", 1 }, { "C", 2 }, { "D", 3 },
-	{ "E", 4 }, { "F", 5 }, { "G", 6 }, { "H", 7 }, { "I", 8 }, { "J", 9 }, 
-	{ "K", 10 }, { "L", 11 }, { "M", 12 }, { "N", 13 }, { "O", 14 }, { "P", 15 }, 
-	{ "Q", 16 }, { "R", 17 }, { "S", 18 }, { "T", 19 }, { "U", 20 }, { "V", 21 }, 
-	{ "W", 22 }, { "X", 23 }, { "Y", 24 }, { "Z", 25 }}; // key: A, value:0 | key:B, value:1 ...
+	map<string, int> m; // key: A, value:0 | key:B, value:1 ...
 	int n;
 
 	vector<string> cnt[51];
@@ -30,18 +26,28 @@ int main(void){
 	{
 		cin >> afta;
 		cin >> n;
+		int mcnt = 0;
+
+		if (!m[afta]) // KEY의 Value가 존재하지 않는 경우, value값 지정해줌
+		{
+			m[aefa] = mcnt++;
+		}
 
 		for (int j = 0; j < n; j++)
 		{
 			string befa;
 			cin >> befa;
-			cnt[m[afta]].push_back(befa);
+			if (!m[befa])
+			{
+				m[befa] = mcnt++;
+			}
+			cnt[m[afta]].push_back(befa); //afta에 연결된 웹사이트의 이름들을 vector에 나열
 			//cout << cnt[m[afta]][j] << " ";
 		}
 	}
 
 	string alp;
-	cin >> alp;
+	cin >> alp;                                                                                                                                                       
 	
 	cout << score(cnt,m,alp) << endl;
 
